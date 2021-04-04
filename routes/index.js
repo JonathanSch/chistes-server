9646df465ee75305e31a2049d5f20d455fc1bf1f
JonathanSch <jonathansch7@gmail.com>
2021-04-03 21:45:47 -0600

    Thigs added

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const Chiste = require('../schema/Chiste')
 
 router.get('/obtener',async (req,res)=>{
-    const chistes = await Chiste.find().limit(5);
+    try {
+        const chistes = await Chiste.find().limit(5);
+        res.send({chistes}).status(200);
+        
+    } catch (error) {
+        console.log(error)
+    }
 
-    res.send({chistes}).status(200);
 })
 
 router.post('/subirChiste',async (req,res)=>{
@@ -14,14 +19,25 @@ router.post('/subirChiste',async (req,res)=>{
         texto:req.body.texto
     });
 
-    const chisteSubido = await nuevoChiste.save();
-    res.send({chisteSubido}).status(201);
+    try {
+        const chisteSubido = await nuevoChiste.save();
+        res.send({chisteSubido}).status(201);
+        
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 router.delete('/borrarChiste',async (req,res)=>{
-    const chisteBorrado = await Chiste.findOneAndDelete({texto:req.body.texto});
+    try {
+        
+        const chisteBorrado = await Chiste.findOneAndDelete({texto:req.body.texto});
+        res.send({chisteBorrado}).status(200);
+    } catch (error) {
+        console.log(error)
+    }
 
-    res.send({chisteBorrado}).status(200);
 })
 
 module.exports = router;
